468a307485b6d1d7e93a8cb7087137e8faba7899
annaAzh <forarre@yandex.ru>
2024-01-04 10:08:56 +0300

    fix(extended - 1.2): fill alt for images

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
     deleteButton.className="task__btn-delete delete";
     deleteButtonImg.className = "task__img-delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.alt = 'button delete';
     deleteButton.appendChild(deleteButtonImg);
 
 
